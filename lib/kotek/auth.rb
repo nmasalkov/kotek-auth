de385d52d907898899031e89ab5679a18573696c
Nikita Masalkov <nikita.masalkov@softswiss.com>
2023-03-16 14:39:44 +0100

    initialize empty gem

---
+++ lib/kotek/auth.rb
@@ -0,0 +1,8 @@
+require "kotek/auth/version"
+
+module Kotek
+  module Auth
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/kotek/auth/version.rb
@@ -0,0 +1,5 @@
+module Kotek
+  module Auth
+    VERSION = "0.1.0"
+  end
+end
